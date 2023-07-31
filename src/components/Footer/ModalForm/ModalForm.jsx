59f48856ef4900e2b32d11e0b18a501af266d981
Vladyslav <vladyslav@MacBook-Air-Vladyslav.local>
2023-07-31 23:11:56 +0300

    update code

--- src/components/Footer/ModalForm/ModalForm.jsx
+++ src/components/Footer/ModalForm/ModalForm.jsx
@@ -14,7 +14,8 @@ import { ReactComponent as SendIcon } from '../../../images/Footer/send.svg';
 
 import scss from '../Footer.module.scss';
 
-axios.defaults.baseURL = 'https://jukrassik-pork.onrender.com/api/contact';
+axios.defaults.baseURL =
+  'https://jukrassik-pork-mailer-backend.onrender.com/api/contact';
 const EMAIL_REGEX =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/; // eslint-disable-line
 
